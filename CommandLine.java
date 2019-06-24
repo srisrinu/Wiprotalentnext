75f73c196903208c02a4f7f0d6585615ff4b8aa2
srisrinu <43072102+srisrinu@users.noreply.github.com>
2019-06-24 14:16:31 +0530

    Add files via upload

---
+++ CommandLine.java
@@ -0,0 +1,4 @@
+public class CommandLine{
+public static void main(String []args){
+System.out.println(args[0]+" Technologies "+args[1]);
+}}
---
+++ commandLine1.java
@@ -0,0 +1,4 @@
+public class CommandLine{
+public static void main(String []args){
+System.out.println("Welcome "+args[0]);
+}}
