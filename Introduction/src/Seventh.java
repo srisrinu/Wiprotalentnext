a0f9d7bdc07e13641f80058d12da1633ed5f35da
srisrinu <srinivasroy2018@gmail.com>
2019-06-26 23:36:38 +0530

    commit

---
+++ Introduction/src/Seventh.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Seventh {
+
+	public static void main(String[] args) {
+	   int Upper,Lower;
+	   char character;
+		Scanner sc=new Scanner(System.in);
+		char s=sc.next().charAt(0);
+		if(s>=65&&s<=90) {
+		   Upper=(int)s;
+		   character=(char)(Upper+32);
+		   System.out.print(s+"->"+character);}
+			else {
+				 Lower=(int)s;
+				   character=(char)(Lower-32);
+				   System.out.println(s+"->"+character);
+				
+			}
+		}
+		
+
+	}
+
+
