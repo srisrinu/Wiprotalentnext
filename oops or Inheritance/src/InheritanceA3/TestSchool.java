5ea8b225b0e647ae5c94e945dba1958fa4071a27
srisrinu <srinivasroy2018@gmail.com>
2019-07-15 15:04:13 +0530

    commit

--- oops or Inheritance/src/InheritanceA3/TestSchool.java
+++ oops or Inheritance/src/InheritanceA3/TestSchool.java
@@ -6,7 +6,7 @@ public class TestSchool {
 		// TODO Auto-generated method stub
      Person p=new Person("srinu","01/03/1999");
      System.out.println(p);
-     Teacher t=new Teacher("Hardy","25/2/1985",122222,"lgorithmic mathematics");
+     Teacher t=new Teacher("Hardy","25/2/1985",122222,"algorithmic mathematics");
      System.out.println(t);
      Student s=new Student("srinu","01/03/1999","160040135");
      System.out.println(s);
