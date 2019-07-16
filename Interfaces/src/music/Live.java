e30d202a9058e50f48abcb0a748dfffbcf27fe5e
srisrinu <srinivasroy2018@gmail.com>
2019-07-16 22:41:47 +0530

    commit

---
+++ Interfaces/src/music/Live.java
@@ -0,0 +1,15 @@
+package music;
+import music.string.Veena;
+import music.wind.*;
+
+public class Live {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Veena veena =new Veena();
+       Saxophone saxophone=new Saxophone();
+       veena.play();
+       saxophone.play();
+	}
+
+}
